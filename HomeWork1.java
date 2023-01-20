318d08ade692cbb7fcf86590e07b15f7e808758b
STAN-65031 <lox.ibany@gmail.com>
2023-01-20 22:40:15 +0300

    task 1 is done

---
+++ HomeWork1.java
@@ -0,0 +1,31 @@
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+package home_work_1;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = iScanner.nextInt();
+        System.out.println("Сумма чисел = " + SumТumbers(number));
+        System.out.println("произведение чисел = " + ProductTumbers(number));
+    }
+    private static int SumТumbers(int number) {
+        int result = 0;
+        for (int i = 1; i < number+1; i++) {
+            result = result + i;   
+        }
+        return result;
+        
+    }
+    private static int ProductTumbers(int number) {
+        int result = 1;
+        for (int i = 1; i < number+1; i++) {
+            result = result * i;   
+        }
+        return result;
+        
+    }
+}
